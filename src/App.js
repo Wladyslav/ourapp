8f2410eac5c85712a62520f0719f0f853c7657c9
Sebskora <sebastian.skora77@gmail.com>
2020-09-29 12:09:28 +0200

    test push

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react'; 
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './layout/header/Header'
-import MainPage from './pages/mainPage/MainPage';
-import Contact from './pages/contact/Contact';
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./layout/header/Header";
+import MainPage from "./pages/mainPage/MainPage";
+import Contact from "./pages/contact/Contact";
 
 class App extends Component {
   state = {
-
-  }
-  render(){
-    return(
+    test: "dziala",
+  };
+  render() {
+    return (
       <BrowserRouter>
         <>
           <Switch>
-            <Header/>
+            <Header />
             <Route exact path="/" component={MainPage} />
             <Route path="/contact" component={Contact} />
           </Switch>
         </>
       </BrowserRouter>
-      
-      
-    )
+    );
   }
 }
-
 
 export default App;
