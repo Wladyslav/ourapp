bba5fe06dae5a2c4c8843bba3935b945ea675b5e
Sebskora <sebastian.skora77@gmail.com>
2020-09-29 22:43:03 +0200

    added started of text carousel mainpage

--- src/pages/mainPage/HeroPage/WordsSlider/WordsSlider.js
+++ src/pages/mainPage/HeroPage/WordsSlider/WordsSlider.js
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
 import Button from "../../../../smallComponents/Button/Button";
 import styles from "./WordsSlider.module.scss";
+
+const textContent = [
+  {
+    mainTitle: "UX DESIGN",
+  },
+  {
+    mainTitle: "MODERN TECHNOLOGIES",
+  },
+  {
+    mainTitle: "WORDPRESS EXPERT",
+  },
+  {
+    mainTitle: "IOS & ANDROID APPS",
+  },
+];
+
 class WordsSlider extends Component {
-  state = {};
+  state = {
+    currentTextId: 0,
+  };
+
+  startCarousel = () => {
+    this.carouselInterval = setInterval(() => {
+      if (this.state.currentTextId < textContent.length - 1) {
+        this.setState((prevState) => ({
+          currentTextId: prevState.currentTextId + 1,
+        }));
+      } else {
+        this.setState(() => ({
+          currentTextId: 0,
+        }));
+      }
+    }, 3000);
+  };
+
+  componentDidMount() {
+    this.startCarousel();
+  }
 
   render() {
     return (
       <div className={styles.mainPage_container}>
         <div className={styles.title_container}>
-          <h1 className={styles.mainTitle}>UX DESIGN</h1>
+          <h1 className={styles.mainTitle}>
+            {textContent[this.state.currentTextId].mainTitle}
+          </h1>
           <h3 className={styles.subTitle}>
             Freelance Web & App Developer based in Brighton, UK
           </h3>
