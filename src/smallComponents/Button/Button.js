0762c1bd3fd5f47bfeaf1070d1bbfc8f76ac66d3
Sebskora <sebastian.skora77@gmail.com>
2020-09-29 16:21:00 +0200

    button scss fixed

--- src/smallComponents/Button/Button.js
+++ src/smallComponents/Button/Button.js
@@ -1,18 +1,17 @@
 import React from "react";
 import styles from "./Button.module.scss";
+
 const Button = ({ children, secondary }) => {
   return (
-    <button
-      style={{
-        backgroundColor: secondary ? "#fff" : "transparent",
-        color: secondary ? "black" : "white",
-        border: "1px solid white",
-        padding: "10px",
-        fontWeight: "700",
-      }}
-    >
-      {children}
-    </button>
+    <>
+      <button
+        className={
+          secondary ? `${styles.mainBtn} ${styles.secondary}` : styles.mainBtn
+        }
+      >
+        {children}
+      </button>
+    </>
   );
 };
 
