241be4ef844ecff3c93c5a52902e34f598effdbf
Sebskora <sebastian.skora77@gmail.com>
2020-10-03 12:49:35 +0200

    added clients opinion section relative

---
+++ src/pages/mainPage/ClientsOpinionSection/ClientsOpinionSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./ClientsOpinionSection.module.scss";
+const ClientsOpinionSection = () => {
+  return <div className={styles.clients_opinion_container}></div>;
+};
+
+export default ClientsOpinionSection;
