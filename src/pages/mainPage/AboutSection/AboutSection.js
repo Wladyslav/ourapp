c8b630db234c4a3430d6800303d7888008cac0f9
Sebskora <sebastian.skora77@gmail.com>
2020-10-05 14:45:08 +0200

    added improvement to hero page title, about animation added, some fixes

--- src/pages/mainPage/AboutSection/AboutSection.js
+++ src/pages/mainPage/AboutSection/AboutSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Fade from "react-reveal/Fade";
 import styles from "./AboutSection.module.scss";
 
 const items_array = [
@@ -26,49 +26,52 @@ const items_array = [
 class AboutSection extends Component {
   render() {
     return (
-      <div className={styles.section_container}>
+      <section className={styles.section_container}>
         <div className={styles.slide_up}>
-          <div className={styles.description_container}>
-            <p className={styles.logo}>
-              <span
-                style={{
-                  color: "#666",
-                }}
-              >
-                S
-              </span>
-              <span style={{ color: "#bda67d" }}>S</span>
-            </p>
-            <p class={styles.description}>
-              Freelance Web & App Developer in Brighton with expertise in both
-              front and back end development, along with solid UI/UX Design
-              skills. Offering a wide range of web and app development services,
-              such as bespoke WordPress sites, eCommerce platforms, iOS and
-              Android apps, and powerful .NET sites...
-            </p>
-          </div>
-          <div className={styles.title}>
-            <p>WEB & APP</p>
-            <p>DEVELOPMENT FEATURES</p>
-          </div>
-          <div className={styles.icon_text_container}>
-            {items_array.map((item) => (
-              <div className={styles.item}>
-                <div className={styles.circle}>
-                  <i
-                    className={item.icon}
-                    style={{ fontSize: "29px", color: "#bfa67a" }}
-                  ></i>
+          <Fade bottom>
+            <div className={styles.description_container}>
+              <p className={styles.logo}>
+                <span
+                  style={{
+                    color: "#666",
+                  }}
+                >
+                  S
+                </span>
+                <span style={{ color: "#bda67d" }}>S</span>
+              </p>
+              <p class={styles.description}>
+                Freelance Web & App Developer in Brighton with expertise in both
+                front and back end development, along with solid UI/UX Design
+                skills. Offering a wide range of web and app development
+                services, such as bespoke WordPress sites, eCommerce platforms,
+                iOS and Android apps, and powerful .NET sites...
+              </p>
+            </div>
+
+            <div className={styles.title}>
+              <p>WEB & APP</p>
+              <p>DEVELOPMENT FEATURES</p>
+            </div>
+            <div className={styles.icon_text_container}>
+              {items_array.map((item) => (
+                <div className={styles.item}>
+                  <div className={styles.circle}>
+                    <i
+                      className={item.icon}
+                      style={{ fontSize: "29px", color: "#bfa67a" }}
+                    ></i>
+                  </div>
+                  <div className={styles.item_text_container}>
+                    <p className={styles.main_text}>{item.main_text}</p>
+                    <p className={styles.sub_text}>{item.sub_text}</p>
+                  </div>
                 </div>
-                <div className={styles.item_text_container}>
-                  <p className={styles.main_text}>{item.main_text}</p>
-                  <p className={styles.sub_text}>{item.sub_text}</p>
-                </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </Fade>
         </div>
-      </div>
+      </section>
     );
   }
 }
