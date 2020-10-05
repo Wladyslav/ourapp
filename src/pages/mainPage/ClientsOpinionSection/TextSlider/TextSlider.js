cf4948a81be6f285841925d16eef4a5ea4a01fb3
Sebskora <sebastian.skora77@gmail.com>
2020-10-05 22:59:38 +0200

    fixed client opinions slider

--- src/pages/mainPage/ClientsOpinionSection/TextSlider/TextSlider.js
+++ src/pages/mainPage/ClientsOpinionSection/TextSlider/TextSlider.js
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./TextSlider.module.scss";
-import Fade from "react-reveal/Fade";
+
 class SimpleSlider extends React.Component {
   render() {
     var settings = {
@@ -15,64 +15,62 @@ class SimpleSlider extends React.Component {
     };
     return (
       <Slider {...settings} autoplay="true" className={styles.slider_container}>
-        <Fade bottom>
-          <article className={styles.slider_item}>
-            <i
-              className="fas fa-quote-left"
-              style={{
-                color: "#fff",
-                textAlign: "center",
-                width: "100%",
-                fontSize: "24px",
-              }}
-            ></i>
-            <h3
-              style={{
-                color: "#fff",
-                textAlign: "center",
-                lineHeight: "39px",
-                margin: "30px 0",
-                fontFamily: "PT Serif, serif",
-                fontSize: "24px",
-                fontStyle: "italic",
-                fontWeight: "100",
-              }}
-            >
-              "Finally, a web developer who never disappointed us"
-              <br />
-              Vladyslaw and Sebastian has always provided good solutions to our
-              web challenges, in a timely manner and frequently pro-actively.
-            </h3>
-          </article>
-          <article className={styles.slider_item}>
-            <i
-              className="fas fa-quote-left"
-              style={{
-                color: "#fff",
-                textAlign: "center",
-                width: "100%",
-                fontSize: "28px",
-              }}
-            ></i>
-            <h3
-              style={{
-                color: "#fff",
-                textAlign: "center",
-                lineHeight: "39px",
-                margin: "30px 0",
-                fontFamily: "PT Serif, serif",
-                fontStyle: "italic",
-                fontSize: "24px",
-                fontWeight: "100",
-              }}
-            >
-              "Vladyslaw and Sebastian are very talented and I would highly
-              recommend him to anyone."
-              <br />
-              Thank you Vladyslaw and Sebastian for all your hard work!
-            </h3>
-          </article>
-        </Fade>
+        <article className={styles.slider_item}>
+          <i
+            className="fas fa-quote-left"
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              width: "100%",
+              fontSize: "24px",
+            }}
+          ></i>
+          <h3
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              lineHeight: "39px",
+              margin: "30px 0",
+              fontFamily: "PT Serif, serif",
+              fontSize: "24px",
+              fontStyle: "italic",
+              fontWeight: "100",
+            }}
+          >
+            "Finally, a web developer who never disappointed us"
+            <br />
+            Vladyslaw and Sebastian has always provided good solutions to our
+            web challenges, in a timely manner and frequently pro-actively.
+          </h3>
+        </article>
+        <article className={styles.slider_item}>
+          <i
+            className="fas fa-quote-left"
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              width: "100%",
+              fontSize: "28px",
+            }}
+          ></i>
+          <h3
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              lineHeight: "39px",
+              margin: "30px 0",
+              fontFamily: "PT Serif, serif",
+              fontStyle: "italic",
+              fontSize: "24px",
+              fontWeight: "100",
+            }}
+          >
+            "Vladyslaw and Sebastian are very talented and I would highly
+            recommend him to anyone."
+            <br />
+            Thank you Vladyslaw and Sebastian for all your hard work!
+          </h3>
+        </article>
       </Slider>
     );
   }
