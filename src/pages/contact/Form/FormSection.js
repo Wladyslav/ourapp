1dacdcabe62a484f42571091933d28db7cf6971a
wladyslaw <wladyslawsahaidak@gmail.com>
2020-10-06 15:54:41 +0200

    Form

---
+++ src/pages/contact/Form/FormSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./FormSection.module.scss";
+
+const FormSection = () => (
+  <section className={styles.contact_form}>
+    <div className={styles.contact_items_container}>
+      <div className={styles.contact_item}>
+        <div className={styles.contact_item_icon}>
+          <i class="fas fa-search-location"></i>
+        </div>
+        <h3></h3>
+        <span></span>
+      </div>
+      <div className={styles.contact_item}>
+        <div className={styles.contact_item_icon}>
+          <i class="fas fa-phone"></i>
+        </div>
+        <h3></h3>
+        <span></span>
+      </div>
+      <div className={styles.contact_item}>
+        <div className={styles.contact_item_icon}>
+          <i class="far fa-envelope"></i>
+        </div>
+        <h3></h3>
+        <span></span>
+      </div>
+    </div>
+  </section>
+);
+export default FormSection;
