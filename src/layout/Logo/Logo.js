7a96430d3dc4a601e62477ac1699202de72c9b14
Sebskora <sebastian.skora77@gmail.com>
2020-10-06 19:12:21 +0200

    added logo , and media on nav

---
+++ src/layout/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: "30px",
+        fontSize: "40px",
+        border: "2px solid white",
+        padding: "5px 12px",
+      }}
+    >
+      <span style={{ color: "#fff" }}>
+        S<span style={{ color: "#909090" }}>S</span>
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -2,9 +2,11 @@ import React from "react";
 import styles from "../header/Header.module.scss";
 import Navigation from "../navigation/Navigation.js";
 import Example from "../mobileNav/Example";
+import Logo from "../Logo/Logo";
 
 const Header = () => (
   <header className={styles.header}>
+    <Logo />
     <Example />
     <Navigation />
   </header>
