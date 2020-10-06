9f21d27485dfd279ed4da94fb9d5a6de46c68164
Sebskora <sebastian.skora77@gmail.com>
2020-10-06 18:53:22 +0200

    fixed footer on contact

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Footer.module.scss";
-const Footer = () => {
+const Footer = ({ position }) => {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footer} style={{ position: position }}>
       © 2020 Sebastian Skóra and Vladyslav Sahaidak | Freelance Web & App
       Developer in Brighton UK
     </footer>
