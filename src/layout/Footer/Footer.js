b8f89a1f2b83d3614de0359e83e4a0a969baf7c0
wladyslaw <wladyslawsahaidak@gmail.com>
2020-11-13 20:30:18 +0100

    icons footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -3,8 +3,21 @@ import styles from "./Footer.module.scss";
 const Footer = ({ position }) => {
   return (
     <footer className={styles.footer} style={{ position: position }}>
-      © 2020 Sebastian Skóra and Vladyslav Sahaidak | Freelance Web & App
+      © 2020 Sebastian Skóra and Władysław Sahaidak | Freelance Web & App
       Developer
+      <div>
+        Icons made by{" "}
+        <a
+          href="https://www.flaticon.com/authors/pixelmeetup"
+          title="Pixelmeetup"
+        >
+          Pixelmeetup
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
     </footer>
   );
 };
